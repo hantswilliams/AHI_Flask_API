13a3c65e86ef0edf3be216f828b8de0f5368f69c
Hants <hantsawilliams@gmail.com>
2020-10-04 12:28:31 -0500

    Added a simple example
    single page

---
+++ Simple_Example.py
@@ -0,0 +1,17 @@
+from flask import Flask
+app = Flask(__name__)
+
+
+## In order to run this, all you need to do is naviagate to the folder in which this 
+## file lives, and then type the follow below and press enter: 
+## python Simple_Example.py 
+
+@app.route('/')
+def hello():
+    return "Hello World!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+## This will start up a 'development' server on your own local machine 
