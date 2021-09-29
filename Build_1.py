a39860491f644775396282aea83349f7a42140b8
hants <hantsawilliams@gmail.com>
2021-09-29 17:26:35 -0400

    added readme changes

--- Build_1.py
+++ Build_1.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello Cadada!"
+    return "Hello 😀!"
 
 if __name__ == '__main__':
     app.run(debug=True)
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 AHI_Flask_API
+
+- To run locally: 
+    - set the name of the version/file you want to run: `export FLASK_APP=Build_1.py` 
+    - `python -m flask run` 
