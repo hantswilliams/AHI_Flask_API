8b688077c3138aa0a67df5844630c8a3c045c605
hants <hantsawilliams@gmail.com>
2021-10-06 17:21:53 -0400

    added host/port to each build

--- Build_1.py
+++ Build_1.py
@@ -15,7 +15,7 @@ def hello():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
 
 
 ## This will start up a 'development' server on your own local machine 
--- Build_3.py
+++ Build_3.py
@@ -81,4 +81,4 @@ def get_patients_greaterthanID(patient_id):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
